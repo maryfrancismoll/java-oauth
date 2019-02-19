712e25843ee761e3fc17b23c01e88f2fff01a3f3
Kitchie <francismoll@gmail.com>
2019-02-20 10:24:47 +1300

    initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@CrossOrigin(origins = "http://localhost:4200")
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
